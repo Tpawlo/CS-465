6f0c7047997aaaece52f2e3bffa7cda3855bdfc8
Tabitha Pawlowski <tabbypcat@hotmail.com>
2025-11-12 13:52:22 -0500

    Mopdule 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,13 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync(__dirname + '/../data/trips.json',
+'utf8'));
+
+
+
 /*GET Travel View*/
 
 const travel = (req, res) => {
-    res.render( 'travel', { title: "Travlr Getaweays"});
+    res.render( 'travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
